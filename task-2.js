1ecff6e74af90925fbe7c1da773ed5f706855f8f
sulayman110 <piceofislam@gmail.com>
2025-08-02 05:56:31 +0600

    tasks-2-solve

---
+++ task-2.js
@@ -0,0 +1,16 @@
+ let BMI=26;
+
+if(BMI < 18.5){
+    console.log('underweight')
+}
+else if(BMI <= 24.5){
+        console.log(' you are normal')
+    }
+
+else if(BMI <= 29.9){
+        console.log('you are overweight')
+    }
+
+else{
+    console.log('you are obese')
+}
