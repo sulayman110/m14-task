cffaaa326e69b34f168d0f40e1acb83f93826f01
sulayman110 <piceofislam@gmail.com>
2025-08-02 16:30:23 +0600

    friends-tasks-5-complit

---
+++ tasks-5.js
@@ -0,0 +1,11 @@
+let num1 = 225 ;
+let num2 = 20 ;
+// let result;
+// if(num1 > num2){
+//   result = num1*2
+// }
+// else{result= num1+num2}
+// console.log('result',result)
+
+let result = (num1 > num2) ? (num1*2) : (num1+num2);
+console.log('result:',result)
