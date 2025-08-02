e7b1d324fde4341f06ec988f049df8f23a91d6bc
sulayman110 <piceofislam@gmail.com>
2025-08-02 13:12:53 +0600

    result-tasks

---
+++ tasks-3.js
@@ -0,0 +1,23 @@
+const result=-25;
+//  A: 90-100
+//     B: 80-89
+//     C: 70-79
+//     D: 60-69
+//     F: 0-59
+
+if(result>=90 && result<=100){
+    console.log( "your Grads is 'A' ");
+}
+else if(result>=80 && result<=89){
+    console.log("your Grads is 'B' ");
+}
+else if(result>=70 && result<=79){
+    console.log("your Grads is 'C' ");
+}
+else if(result>=60 && result<=69){
+    console.log("your Grads is 'D' ");
+}
+else if(result>=0 && result<=59){
+    console.log("your Grads is 'F' ");
+}
+else{console.log('insart yore Number pless inpu 0 to 100')}
