d311cd3e4d00da1435ffba752ee588febffdb5f5
sulayman110 <piceofislam@gmail.com>
2025-08-03 03:31:01 +0600

    ternary-pactic

--- tasks-6.js
+++ tasks-6.js
@@ -6,10 +6,14 @@ Ticket fare Calculator
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
-let ticketPrice = 800 ;
-let age =;
-const students = true ;
-
-if(age < 10){
-    console.log(free)
+let age = ;
+let isStrudent = 25 ;
+let price  = 800;
+if(age < 10){     //free
+    price=0 ;
 }
+else if(isStrudent === true){
+  
+}
+
+console.log('Ticket price',price,'tk');
---
+++ ternary.js
@@ -0,0 +1,41 @@
+// const age = 12 ;
+// if(age > 18){
+//     console.log('my rob allah')
+// }
+// else{
+//     console.log('subhan allah')
+// }
+// age>18?console.log('my rob allah'):console.log('subhan allah');
+
+// let price = 500;
+// const isLeader = false ;
+// // if(isLeader === true){
+// //     price=0;
+        
+// //     }
+// //     else{
+// //         price=price+100;
+// //     }
+// //     console.log(price)
+
+
+//  price = isLeader=== true ? 0 : (price + 100);
+//  console.log(price)
+
+
+
+let price = 900 ;
+const isLeader = true;
+// if(isLeader === true){
+//     if(price > 1000){
+//         price = price/2;
+//     }else{
+//         price=0;
+//     }
+// }
+// else{price=price+500;}
+// console.log(price)
+
+price= isLeader === true ? price > 1000 ? price = price/2: 0 :price + 500 ;
+
+console.log(price)
